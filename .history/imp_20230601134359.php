068cecb589d6a39882feed954c4d493d8640b1e9
Sandhya Rimal <71540130+sandhya8109@users.noreply.github.com>
2023-06-01 13:48:13 +0545

    initial commit

---
+++ .history/imp_20230601134359.php
@@ -0,0 +1,140 @@
+<?php
+include('nav.php')
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Hope Ability</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start 
+    <div id="spinner"
+        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
+    </div>
+   Spinner End -->
+
+
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container text-center py-5">
+            <h1 class="display-2 text-white mb-4 animated slideInDown">Important</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item text-primary" aria-current="page">Important</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- Page Header End -->
+    <!-- Start -->
+    <div class="container-xxl py-5">
+        <h2 class="text-center">Important Documents</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="row text-center">
+                        <div class="col-md-4">
+                            <i class="fa fa-3x fa-regular fa-file-pdf  mb-4"></i>
+                            <h5 class="mb-3">Hope Ability Brochure</h5>
+                            <a href="./imodoc/Hope Ability Brochure.pdf" target=”_blank”>
+                                <button class="bg-danger text-light" style="border-color: red;padding: 10px;">Download
+                                    Now</button></a>
+                        </div>
+                        <div class="col-md-4"><i class="fa fa-3x fa-regular fa-file-pdf  mb-4"></i>
+                            <h5 class="mb-3">Participant-Handbook</h5>
+                            <a href="https://www.hopeability.com.au/wp-content/uploads/2021/11/Hope-Ability-Participant-Handbook-1.pdf"
+                                target=”_blank”>
+                                <button class="bg-danger text-light" style="border-color: red;padding: 10px;">Download
+                                    Now</button></a>
+                        </div>
+                        <div class="col-md-4">
+                            <i class="fa fa-3x fa-regular fa-file-pdf  mb-4"></i>
+                            <h5 class="mb-3">Privacy-Statements</h5>
+                            <a href="https://www.hopeability.com.au/wp-content/uploads/2021/11/Privacy-Statement-1.pdf"
+                                target=”_blank”>
+                                <button class="bg-danger text-light" style="border-color: red;padding: 10px;">Download
+                                    Now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="row text-center">
+                        <div class="col-md-6">
+                            <i class="fa fa-3x fa-regular fa-file-pdf  mb-4"></i>
+                            <h5 class="mb-3">Easy-Read-Complaints</h5>
+                            <a href="https://www.hopeability.com.au/wp-content/uploads/2021/11/Easy-Read-Complaints-1.pdf"
+                                target=”_blank”>
+                                <button class="bg-danger text-light" style="border-color: red;padding: 10px;">Download
+                                    Now</button></a>
+
+                        </div>
+                        <div class="col-md-6">
+                            <i class="fa fa-3x fa-regular fa-file-pdf  mb-4"></i>
+                            <h5 class="mb-3">Complaints-Feedback</h5>
+                            <a href="https://www.hopeability.com.au/wp-content/uploads/2021/11/Complaints-Feedback-Form-2.pdf"
+                                target=”_blank”>
+                                <button class="bg-danger text-light" style="border-color: red;padding: 10px;">Download
+                                    Now</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end-->
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
+            class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+<?php
+include('footer1.php')
+    ?>
+
+</html>
