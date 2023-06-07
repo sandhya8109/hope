fca5eeaa1e12a71ba96f2cb329b14615d849fc52
Kabita57 <131524298+Kabita57@users.noreply.github.com>
2023-06-07 17:21:39 +0545

    initial commit

--- aboutus.php
+++ aboutus.php
@@ -13,8 +13,26 @@ include('nav.php')
             </ol>
         </nav>
     </div>
+<<<<<<< Updated upstream
 </div>
 <!-- Page Header End -->
+=======
+   Spinner End -->
+    <!-- Page Header Start -->
+    <div class="container-fluid pageabout-header py-5 mb-5 wow fadeIn"  data-wow-delay="0.1s">
+        <div class="container text-center py-5">
+            <h1 class="display-2 text-white mb-4 animated slideInDown">About Us</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item text-primary" aria-current="page">About us</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- Page Header End -->
+>>>>>>> Stashed changes
 
 <!-- About Start -->
 <div class="container-xxl py-5">
